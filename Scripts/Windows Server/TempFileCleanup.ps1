8fd8245cc4e972071b5bb6b1ac2a312a4b5f5bf1
LogicalDistruction <71558075+LogicalDistruction@users.noreply.github.com>
2024-02-12 13:25:20 -0600

    Create TempFileCleanup.ps1

---
+++ Scripts/Windows Server/TempFileCleanup.ps1
@@ -0,0 +1,15 @@
+##########################################
+# Author: Robert Stacks
+# Date: 7/13/2023
+#
+# Last Updated by: Robert Stacks
+# Last Update Date: 7/13/2023
+#
+# Purpose of Script: To clean up typical temp directories found on a Windows Server
+#                    You might consider using task scheduler to run this once a month, 
+#                    or once a day or whatever your needs may be.
+###############################################################################################
+
+$tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Users\*\Appdata\Local\Temp\*","C:\Users\*\AppData\Roaming\Microsoft\Windows\Recent Items\*")
+
+Get-ChildItem -File -Recurse -ErrorAction SilentlyContinue -Path $tempfolders | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-10))} | Remove-Item -force -Recurse
