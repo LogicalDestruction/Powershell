bbdca8239ed4e8e2f5d22460680144b48d8d0585
LogicalDistruction <71558075+LogicalDistruction@users.noreply.github.com>
2024-02-12 13:27:42 -0600

    Update TempFileCleanup.ps1

--- Scripts/Windows Server/TempFileCleanup.ps1
+++ Scripts/Windows Server/TempFileCleanup.ps1
@@ -1,4 +1,4 @@
-###############################################################################################
+<##############################################################################################
 # Author: Robert Stacks
 # Date: 7/13/2023
 # Last Update Date: 7/13/2023
@@ -7,7 +7,7 @@
 #                    To clean up typical temp directories found on a Windows Server
 #                    You might consider using task scheduler to run this once a month, 
 #                    or once a day or whatever your needs may be.
-###############################################################################################
+###############################################################################################>
 
 $tempfolders = @("C:\Windows\Temp\*", "C:\Windows\Prefetch\*", "C:\Users\*\Appdata\Local\Temp\*","C:\Users\*\AppData\Roaming\Microsoft\Windows\Recent Items\*")
 
